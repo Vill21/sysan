2acf42ec0f786115d3f873937dc81b3604940acd
Vill21 <m1903800@edu.misis.ru>
2022-11-14 21:28:29 +0300

    Added task4 and fixed the sys path error

--- task4/task4.py
+++ task4/task4.py
@@ -4,11 +4,10 @@ ROOT = os.path.abspath(os.pardir)
 
 sys.path.insert(1, ROOT)
 
-from modules import r1, r2, r3, r4, r5, GraphNodeLevel, translate_csv
+from modules import H, l_matrix, GraphNodeLevel, translate_csv
 import queue
 
-def task(csv_file) -> list[list[str]]:
-    """ Возвращает массив отношений для пар вершин системы вида дерева. """
+def task(csv_file):
 
     nodes = translate_csv(csv_file)
 
@@ -43,25 +42,16 @@ def task(csv_file) -> list[list[str]]:
         for child in pointer.children:
             q.put(child)
 
-    # getting relations
-    r1_dict = r1(data_node.get(nodes[0][0]))
-    r2_dict = r2(data_node.get(nodes[0][0]))
-    r3_dict = r3(data_node.get(nodes[0][0]))
-    r4_dict = r4(data_node.get(nodes[0][0]))
-    r5_dict = r5(data_node.get(nodes[0][0]))
+    l = l_matrix([key for key in node_data], 5)
+    entropy = H(l, 5, 2)
 
-    def get_r_list(rk_dict: dict) -> list[list[str]]:
-        output = []
-        for key in rk_dict:
-            output.append(node_data.get(key))
-        
-        return output
+    return entropy
 
-    output = []
-    output.append(get_r_list(r1_dict))
-    output.append(get_r_list(r2_dict))
-    output.append(get_r_list(r3_dict))
-    output.append(get_r_list(r4_dict))
-    output.append(get_r_list(r5_dict))
+if __name__ == '__main__':
+    csv_str = '''1,2
+    1,3
+    3,4
+    3,5'''
 
-    return output
+    entropy = task(csv_str)
+    print(entropy)
